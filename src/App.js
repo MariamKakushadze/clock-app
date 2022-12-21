c4430a5c4adef32f2e00fdcc5bf61c968014285d
MariamK <102153226+MariamKakushadze@users.noreply.github.com>
2022-12-21 19:01:13 +0400

    made quotes component, quotes changes randomly after clicking btn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Quotes from "./components/quotes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[url('./assets/mobile/bg-image-daytime.jpg')] bg-[#D8D8D8] bg-no-repeat bg-cover h-screen w-full bg-blend-multiply p-6">
+      <Quotes />
     </div>
   );
 }
