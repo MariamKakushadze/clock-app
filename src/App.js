f8493ee7e7634887955f2da536a5367cbd9a05fa
MariamK <102153226+MariamKakushadze@users.noreply.github.com>
2022-12-22 21:01:24 +0400

    addind day/night mode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import Quotes from "./components/quotes";
 import Timezone from "./components/timezone";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [daytime, setDaytime] = useState(false);
+
+  useEffect(() => {
+    let hours = new Date().getHours();
+    const setTime = () => {
+      if (hours >= 5 && hours < 18) {
+        setDaytime(true);
+      }
+    };
+    setTime();
+  }, []);
+
   return (
-    <div className="bg-[url('./assets/mobile/bg-image-daytime.jpg')] bg-[#D8D8D8] bg-no-repeat bg-cover h-screen w-full bg-blend-multiply p-6">
+    <div
+      className={`${
+        daytime
+          ? "bg-[url('./assets/mobile/bg-image-daytime.jpg')] md:bg-[url('./assets/tablet/bg-image-daytime.jpg')] xl:bg-[url('./assets/desktop/bg-image-daytime.jpg')]"
+          : "bg-[url('./assets/mobile/bg-image-nighttime.jpg')] md:bg-[url('./assets/tablet/bg-image-nighttime.jpg')] xl:bg-[url('./assets/desktop/bg-image-nighttime.jpg')]"
+      }
+      md:${
+        daytime
+          ? "bg-[url('./assets/tablet/bg-image-daytime.jpg')]"
+          : "bg-[url('./assets/tablet/bg-image-nighttime.jpg')]"
+      }
+       bg-[#D8D8D8] bg-no-repeat bg-cover h-screen w-full bg-blend-multiply p-6 md:p-16`}
+    >
       <Quotes />
       <Timezone />
     </div>
